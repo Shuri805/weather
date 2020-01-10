99d1874d4ac7c5e162e94c8b0cf5e0891b4f1267
Jasmine Harris <jasminecrooms@gmail.com>
2020-01-09 19:47:38 -0800

    added css file and modified js file

--- weather.js
+++ weather.js
@@ -1,4 +1,7 @@
-async function fetchData() {
-  const currentWeather = fetch('');
-}
-fetchData();
+fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=f5da1ec54f0a6fb94126041b294bd58e')
+.then(response => response.json())
+.then(data => {
+  console.log(data)
+  return data.list.weather
+})
+
